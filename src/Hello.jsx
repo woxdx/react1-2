670a6bacc396f379a15a5c8a6d7d6954928e527a
woxdx <lwd0309@gmail.com>
2024-03-27 16:50:56 +0900

    3월 27일 강의내용

---
+++ src/Hello.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Hello() {
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+
+//export default Hello
+
+//class Hello extends React.Component {
+//    render() {
+//        return(
+//            <h1>Hello</h1>
+//        )
+//    }
+//}
---
+++ src/chapter_04/Clock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Clock() {
+    return (
+        <>
+            <h1>안녕, 리액트</h1>
+            <h2>현재 시간 : {new Date().toLocaleTimeString()}</h2>
+        </>
+    )
+}
+
